18f3cf71e15fedb16eb711585d7c84dbeb091870
diego.garbau <diego.garbau@gmail.com>
2019-07-22 09:17:35 +0200

    added UserController

---
+++ src/main/java/com/sos/facemash/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.sos.facemash.controller;
+
+import com.sos.facemash.DTO.UserDetailDTO;
+import com.sos.facemash.DTO.UserInputDTO;
+import com.sos.facemash.DTO.UsersDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "User")
+public interface UserController {
+    @ApiOperation(value = "User", notes = "Endpoint to get all the users", response = UsersDTO.class)
+    UsersDTO getAllUsers();
+
+    @ApiOperation(value = "User", notes = "Endpoint to get an user by userName", response = UserDetailDTO.class)
+    UserDetailDTO getUser(String userName);
+
+    @ApiOperation(value = "User", notes = "Endpoint to create a user", response = UserDetailDTO.class)
+    UserDetailDTO createUser(UserInputDTO userInputDTO);
+
+    @ApiOperation(value = "User", notes = "Endpoint to modify a previous user", response = UserDetailDTO.class)
+    UserDetailDTO modifyUser(String userName, UserInputDTO userInputDTO);
+
+    @ApiOperation(value = "User", notes = "Endpoint to delete a previous user by userName")
+    void deleteUser(String userName);
+}
